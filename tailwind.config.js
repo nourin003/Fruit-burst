6b9a6aedfbb763a732e0cf6aa26c74149b20d024
nourin003 <nourinhasan003@gmail.com>
2023-07-27 01:06:34 +0600

    send new file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {},
+      backgroundImage: {
+        'team-pattern': "url('/images')",
+        
+      }
     },
     plugins: [],
   }
